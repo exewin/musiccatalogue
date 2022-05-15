0a8ddd09807998fbb25493e1f3666ffc47e08451
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-05-15 22:31:40 +0200

    extract column filters

---
+++ src/utils/filterStylesData.js
@@ -0,0 +1,15 @@
+export const filterStylesData = [
+    {
+      text: 'Downtempo',
+      value: 'Downtempo',
+      color: 'purple'
+    },
+    {
+      text: 'Ambient',
+      value: 'Ambient',
+      color: 'volcano'
+    },
+]
+
+
+//magenta red volcano orange gold lime green cyan blue geekblue purple
