6557784728dbe24a13020842e2c5e2cde1d8a81b
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-05-15 22:33:52 +0200

    rework create discogs song

--- src/utils/createDiscogsSong.js
+++ src/utils/createDiscogsSong.js
@@ -1,12 +1,13 @@
-import { grabIdFromUrl } from "./grabIdFromUrl"
+import { grabIdFromYoutubeUrl } from "./grabIdFromUrl"
 export const createDiscogsSong = (data) => {
     const song = {
-        url: data.hasOwnProperty('videos') ? grabIdFromUrl(data.videos[0].uri) : "",
+        url: data.hasOwnProperty('videos') ? grabIdFromYoutubeUrl(data.videos[0].uri) : "",
         artist: data.artists[0].name,
         title: data.title,
         year: data.year,
-        genre: data.genres[0],
-        style: data.styles[0],
+        genres: data.genres,
+        styles: data.styles,
+        discogsUrl: data.id,
     }
     return song
 }
