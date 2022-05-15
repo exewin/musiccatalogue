3c048c1e9adb353c15259e2df06d5197552bea4e
Łukasz Jasiński <windowsmemphis@gmail.com>
2022-05-15 22:30:31 +0200

    add axios to edit.js

--- src/sites/dashboard/Edit.js
+++ src/sites/dashboard/Edit.js
@@ -5,8 +5,10 @@ import {databaseAddSong, databaseGetConcreteSong} from "../../database"
 import {useSearchParams} from "react-router-dom";
 import { useNavigate } from "react-router"
 import { Button, Input, Form, message, InputNumber, Checkbox } from "antd";
-import { grabIdFromUrl } from "../../utils/grabIdFromUrl";
+import { grabIdFromYoutubeUrl, grabIdFromDiscogsUrl } from "../../utils/grabIdFromUrl";
 import { createDiscogsSong } from "../../utils/createDiscogsSong";
+import { splitAndTrim } from "../../utils/splitAndTrim";
+
 
 const Container = styled.div`
     
@@ -17,6 +19,7 @@ const Edit = () => {
     const navigate = useNavigate()
     const [searchParams] = useSearchParams()
     const idParam = searchParams.get('id')
+    const axios = require('axios').default;
 
     const [stayOnPage, setStayOnPage] = useState(false)
 
@@ -27,7 +30,6 @@ const Edit = () => {
     const year = Form.useWatch('year', form);
     const style = Form.useWatch('style', form);
     const genre = Form.useWatch('genre', form);
-
     const discogsUrl = Form.useWatch('discogsUrl', form);
 
     const [songEdit, setSongEdit] = useState({})
@@ -41,44 +43,37 @@ const Edit = () => {
         fillForm(songEdit)
     },[songEdit])
 
-    const fillForm = ({url, title, artist, year, genre, style}) => {
+    const fillForm = ({url, title, artist, year, genres, styles, discogsUrl}) => {
         form.setFieldsValue({
             url: url,
             title: title,
             artist: artist,
             year: year,
-            genre: genre,
-            style: style,
+            genre: genres && genres.join(","),
+            style: styles && styles.join(","),
+            discogsUrl: discogsUrl,
         });
     }
 
-
     const sendData = () => {
-        const songObject = {artist, title, url, year, genre, style}
+        const songObject = {artist, title, url: grabIdFromYoutubeUrl(url), year, genres: splitAndTrim(genre), styles: splitAndTrim(style), discogsUrl: grabIdFromDiscogsUrl(discogsUrl)}
         databaseAddSong(user.userData.login, songObject, idParam)
         if(!stayOnPage)
             navigate(`/dashboard/songs`)
     }
 
-    const convertUrl = () => {
-        form.setFieldsValue({
-            url: grabIdFromUrl(url)
-        });
-    }
+    const fetchFromDiscogs = async() => {
+        try {
+            const response = await axios.get(`https://api.discogs.com/releases/${grabIdFromDiscogsUrl(discogsUrl)}`)
+            fillForm(createDiscogsSong(response.data))
+        } catch (error) {
+            message.error('invalid ID')
+        }
+      }
 
-    const fetchFromDiscogs = () => {
-        fetch(`https://api.discogs.com/releases/${discogsUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            const obj = createDiscogsSong(data)
-            fillForm(obj)
-        })
-    }
 
     const convertUrlBtn = (
-        <Button onClick={convertUrl} size="small" title="This will pull out the ID from full link"> 
-            Convert URL 
-        </Button>
+        <Button size="small" onClick = {fetchFromDiscogs}>Fetch</Button>
     )
 
     const onFinish = () => sendData()
@@ -108,8 +103,13 @@ const Edit = () => {
                     <Input style={{ width: '50%' }} placeholder="example: Blue Monday"/>
                 </Form.Item>
 
-                <Form.Item label="Youtube ID" name="url">
-                    <Input style={{ width: '75%' }} placeholder="example: FYH8DsU2WCk" addonAfter={convertUrlBtn}/>
+                <Form.Item label="Youtube ID" name="url" tooltip="Full links are also accepted">
+                    <Input style={{ width: '75%' }} placeholder="example: FYH8DsU2WCk"/>
+                </Form.Item>
+
+                <Form.Item label="Discogs ID" name="discogsUrl" 
+                tooltip="Full links are also accepted. MUST be single release, not master. Click fetch to automatically fill info based on ID">
+                    <Input style={{ width: '75%' }} placeholder="example: 360760" addonAfter={convertUrlBtn}/>
                 </Form.Item>
 
                 <Form.Item 
@@ -124,15 +124,12 @@ const Edit = () => {
                     />
                 </Form.Item>
 
-                <Form.Item label="Genre / Style">
-                    <Input.Group compact>
-                        <Form.Item name="genre">
-                            <Input placeholder="example: Electronic"/>
-                        </Form.Item>
-                        <Form.Item name="style">
-                            <Input placeholder="example: Synth-pop"/>
-                        </Form.Item>
-                    </Input.Group>
+                <Form.Item label="Genres" name="genre" tooltip="Separate multiple genres with commas: ,">
+                    <Input placeholder="example: Electronic"/>
+                </Form.Item>
+
+                <Form.Item label="Styles" name="style" tooltip="Separate multiple styles with commas: ,">
+                    <Input placeholder="example: Synth-pop"/>
                 </Form.Item>
 
 
@@ -142,18 +139,10 @@ const Edit = () => {
                     </Button>
                 </Form.Item>
 
-                <Checkbox checked={stayOnPage} onChange={()=>setStayOnPage(!stayOnPage)}>Stay on page</Checkbox>
-
-            <hr/>
-
-            Experimental:
-            <Form.Item name="discogsUrl">
-                <Input placeholder="discogs ID"
-                value = ""/>
-            </Form.Item>
+                <Form.Item>
+                    <Checkbox checked={stayOnPage} onChange={()=>setStayOnPage(!stayOnPage)}>Stay on page</Checkbox>
+                </Form.Item>
             
-            <Button type="primary" onClick = {fetchFromDiscogs}>Fetch</Button>
-
             </Form>
 
         </Container>
